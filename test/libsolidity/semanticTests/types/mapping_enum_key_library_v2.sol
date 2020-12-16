60cc9be0489dcadf111688bf06ea43f25403ece5
Alex Beregszaszi <alex@rtfs.hu>
2020-12-16 21:14:52 +0000

    f

--- test/libsolidity/semanticTests/types/mapping_enum_key_library_v2.sol
+++ test/libsolidity/semanticTests/types/mapping_enum_key_library_v2.sol
@@ -1,3 +1,5 @@
+pragma abicoder v2;
+
 enum E { A, B, C }
 library L {
     function get(mapping(E => uint8) storage table, E k) external returns (uint8) {
@@ -17,22 +19,22 @@ contract test {
     }
 }
 // ====
+// compileViaYul: also
 // EVMVersion: >=byzantium
-// ABIEncoderV1Only: true
 // ----
 // library: L
 // get(uint8): 0 -> 0
 // get(uint8): 0x01 -> 0
-// get(uint8): 0xa7 -> FAILURE, hex"4e487b71", 33
+// get(uint8): 0xa7 -> FAILURE
 // set(uint8,uint8): 0x01, 0xa1 ->
 // get(uint8): 0 -> 0
 // get(uint8): 0x01 -> 0xa1
-// get(uint8): 0xa7 -> FAILURE, hex"4e487b71", 33
+// get(uint8): 0xa7 -> FAILURE
 // set(uint8,uint8): 0x00, 0xef ->
 // get(uint8): 0 -> 0xef
 // get(uint8): 0x01 -> 0xa1
-// get(uint8): 0xa7 -> FAILURE, hex"4e487b71", 33
+// get(uint8): 0xa7 -> FAILURE
 // set(uint8,uint8): 0x01, 0x05 ->
 // get(uint8): 0 -> 0xef
 // get(uint8): 0x01 -> 0x05
-// get(uint8): 0xa7 -> FAILURE, hex"4e487b71", 33
+// get(uint8): 0xa7 -> FAILURE
